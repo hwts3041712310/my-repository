24610f7760e3a9a7885cfff22450ba4022a5a73e
hwts3041712310 <115603382+hwts3041712310@users.noreply.github.com>
2023-09-13 16:20:06 +0800

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,17 +1,7 @@
-def big(a,b,c):
-    if(a>b):
-        if(a>c):
-            return a
-        else:
-            return c
-    else:
-        if(b<c):
-            return c
-        else:
-            return b
-
-
-x=input()
-y=input()
-z=input()
-print(big(x,y,z))
+x=int(input())
+y=int(input())
+z=int(input())
+l=[x,y,z]
+l.sort()
+for i in l:
+    print(i)
