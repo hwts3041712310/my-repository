6fe32eeb6bd827e255cf12fec98d1b42a2e23667
hwts3041712310 <115603382+hwts3041712310@users.noreply.github.com>
2023-09-24 11:04:47 +0800

    Update 5.py

--- hw2/5.py
+++ hw2/5.py
@@ -1,9 +1,8 @@
-def Square_root_3():
-    c=2
-    g=c/4
-    i=0
-    while(abs(g*g-c)>0.00000000001):
-        g=( g + c/g)/2
-        i=i+1
-        print("%d:%.13f"%(i,g))
-Square_root_3()
+c=2
+g=c/4
+i=0
+while(abs(g*g-c)>0.00000000001):
+    g=( g + c/g)/2
+    i=i+1
+    print("%d:%.13f"%(i,g))
+
