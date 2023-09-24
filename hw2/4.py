bbad6af5c40cc0b509b437eff5515f0ea2dc3de4
hwts3041712310 <115603382+hwts3041712310@users.noreply.github.com>
2023-09-24 11:04:11 +0800

    Update 4.py

--- hw2/4.py
+++ hw2/4.py
@@ -1,11 +1,9 @@
-def Square_root_1():
-    c=2
-    g=0
-    for j in range(0,c+1):
-        if(j*j>c and g == 0):
-            g = j-1
+c=2
+g=0
+for j in range(0,c+1):
+    if(j*j>c and g == 0):
+        g = j-1
     while(abs(g*g - c) > 0.001):
         g += 0.001
-    print(g)
-Square_root_1()
+print(g)
 
